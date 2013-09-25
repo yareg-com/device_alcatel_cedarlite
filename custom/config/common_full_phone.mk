28005dcf898455f1a830b97fae0fabfa14bd2eb0
Yar Rick <yar@yareg.com>
2013-09-25 11:18:37 +0400

    custom

---
+++ custom/config/common_full_phone.mk
@@ -0,0 +1,18 @@
+# Inherit common CM stuff
+$(call inherit-product, $(LOCAL_PATH)/custom/config/common.mk)
+
+# Bring in all audio files
+include frameworks/base/data/sounds/NewAudio.mk
+
+# Include CM audio files
+include vendor/cm/config/cm_audio.mk
+
+# Default ringtone
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=CyanTone.ogg \
+    ro.config.notification_sound=CyanMessage.ogg \
+    ro.config.alarm_alert=CyanAlarm.ogg
+
+# BT config
+PRODUCT_COPY_FILES += \
+    system/bluetooth/data/main.conf:system/etc/bluetooth/main.conf
