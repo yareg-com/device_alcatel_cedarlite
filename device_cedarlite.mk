a42b38459424f8f28dd15063e76158b5a2c7f8c1
Yar Rick <yar@yareg.com>
2014-07-01 10:30:11 +0400

    low RAM device

--- device_cedarlite.mk
+++ device_cedarlite.mk
@@ -298,6 +298,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # OVERRIDES ----------------------------------------------------------------------------------------
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.low_ram=true \
     ro.setupwizard.enable_bypass=1 \
     ro.opengles.version=131072 \
     hwui.render_dirty_regions=false \
