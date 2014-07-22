106f09b13f3b7dd2ea5ca648a5eb529b4b4d0557
Yar <yar@yareg.com>
2014-07-22 23:04:44 +0400

    merge

--- custom/config/common_full_phone.mk
+++ custom/config/common_full_phone.mk
@@ -1,5 +1,4 @@
 # Inherit common CM stuff
-<<<<<<< HEAD
 $(call inherit-product, $(LOCAL_PATH)/common_full.mk)
 
 # Default notification/alarm sounds
@@ -13,22 +12,3 @@ ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
 endif
 
 $(call inherit-product, $(LOCAL_PATH)/telephony.mk)
-=======
-$(call inherit-product, $(LOCAL_PATH)/common.mk)
-
-# Bring in all audio files
-include frameworks/base/data/sounds/OriginalAudio.mk
-
-# Include CM audio files
-include vendor/cm/config/cm_audio.mk
-
-# Default ringtone
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=CyanTone.ogg \
-    ro.config.notification_sound=CyanMessage.ogg \
-    ro.config.alarm_alert=CyanAlarm.ogg
-
-# BT config
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.conf:system/etc/bluetooth/main.conf
->>>>>>> 19db8e59b86a825ffd157b213f86663510e2c6dc
